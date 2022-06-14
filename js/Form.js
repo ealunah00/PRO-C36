4c01ec80407362a4549eece3205d860263db564c
ealunah00 <96490690+ealunah00@users.noreply.github.com>
2022-06-14 14:36:22 -0500

    PRO-C36
    PRO-C36

---
+++ js/Form.js
@@ -0,0 +1,43 @@
+var input;
+var playButton;
+var titleImg;
+var greeting;
+
+class Form {
+
+  constructor() {
+    this.input = createInput("").attribute("placeholder", "Ingresa tu nombre");
+    this.playButton = createButton("jugar");
+    this.titleImg = createImg("./assets/title.png", "Titulo del juego");
+    this.greeting = createElement("h2");
+  }
+
+  hide() {
+    this.greeting.hide();
+    this.playButton.hide();
+    this.input.hide();
+  }
+
+  //  MÉTODO setElementsPosition
+  
+  
+  
+  
+  
+  
+
+  //  MÉTODO setElementsStyle
+  
+  
+  
+  
+  
+  
+
+  //  MÉTODO display
+  
+  
+  
+  
+
+}
